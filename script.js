81fdf5b9adaf5a2ae589dbaeaec2478c7a03bfe9
Prashwar Mishra <prashwarmishra@gmail.com>
2021-03-20 00:09:09 +0530

    add v2

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+let currentDate=new Date;
+let dateSelected=currentDate.getFullYear()+"-"+(currentDate.getMonth()+1)+"-"+currentDate.getDate();
+$('#date-set').html(dateSelected);
+function getDate(event){
+    event.preventDefault();
+    dateSelected = $('#ar-date').val();
+    $('#date-set').html(dateSelected);
+    fetch();
+}
 
 function updateChanges(data){
+    //empty the previously stored elements
+    $('.img-container').empty();
+    $('.img-description').empty();
+
     const img = $(document.createElement('img'));
     const exp = $(document.createElement('p'));
 
@@ -14,11 +27,18 @@ function updateChanges(data){
     exp.appendTo($('.img-description'));
 }
 
-$.ajax({
-    'url': "https://api.nasa.gov/planetary/apod",
-    'method': 'GET',
-    'success': updateChanges, 
-    'data':{
-        api_key: 'henBDa1fHwN7iODeAlbVnBeJrnPO2g0ht5XN4YKD',
-    }
-});
+function fetch(){
+    $.ajax({
+        'url': "https://api.nasa.gov/planetary/apod",
+        'method': 'GET',
+        'success': updateChanges, 
+        'data':{
+            api_key: 'henBDa1fHwN7iODeAlbVnBeJrnPO2g0ht5XN4YKD',
+            date: dateSelected,
+        }
+    });
+}
+fetch();
+$('#ar-date-btn').click(getDate);
+
+console.log("cycle-end");
